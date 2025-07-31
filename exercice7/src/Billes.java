7a3a7984121d87ae8c343fc1f2f62826c90bf5d6
pierrotdx <pierrotdx@gmail.com>
2025-07-31 18:51:19 +0200

    exercice 7

---
+++ exercice7/src/Billes.java
@@ -0,0 +1,12 @@
+public class Billes {
+    public static void main(String[] args) {
+    int nbBilles = 10;
+        System.out.println("Nombre de billes en début de partie :");
+        System.out.println(nbBilles);
+        nbBilles--;
+        nbBilles--;
+        nbBilles++;
+        System.out.println("Nombre de billes en fin de partie :");
+        System.out.println(nbBilles);
+    }
+}
