aca7f83da3800b273ba3b711e9f8950f87b61569
pierrotdx <pierrotdx@gmail.com>
2025-08-01 17:53:08 +0200

    exercice 24

---
+++ exercice24/src/TrivialPursuit.java
@@ -0,0 +1,13 @@
+public class TrivialPursuit {
+    PlateauJeu plateauDeJeu;
+    De de;
+    Pion pionJaune;
+    Pion pionVert;
+    Pion pionOrange;
+    Pion pionBleu;
+    Pion pionMarron;
+    Pion pionRose;
+    Carte carte;
+
+    void afficheObjetEnAttribut() {}
+}
